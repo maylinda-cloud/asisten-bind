cfeee12316200b4570cd05c605694ba46a194976
maylinda-cloud <maylinda.mudawamah48@sma.belajar.id>
2026-01-30 16:34:23 +0700

    script.js

--- script.js
+++ script.js
@@ -1,20 +1,281 @@
 const kamus = {
-  "aku": "Saya",
-  "gpp": "tidak apa-apa",
-  "ga": "tidak",
-  "nggak": "tidak",
-  "tp": "tetapi",
-  "makasih": "terima kasih",
-  "yaa": "ya",
-  "yg": "yang",
-  "kalo": "kalau",
-  "aja": "saja",
-  "bgt": "banget",
-  "trs": "terus",
-  "tdk": "tidak",
-  "krn": "karena",
-  "dr": "dari",
-  "utk": "untuk"
+  // ===== TAMBAHAN BATCH 1 =====
+
+// kata ganti & sapaan
+"diriku": "saya",
+"elu": "kamu",
+"ente": "kamu",
+
+// kata tanya & penghubung
+"knp": "kenapa",
+"kenapa": "mengapa",
+"gimana": "bagaimana",
+"gmna": "bagaimana",
+"dimana": "di mana",
+"kemana": "ke mana",
+
+// kata sifat (deskriptif)
+"ribet": "rumit",
+"simple": "sederhana",
+"berantakan": "tidak rapi",
+"random": "acak",
+"aneh": "tidak biasa",
+"lebay": "berlebihan",
+"norak": "kurang pantas",
+"kece": "menarik",
+"keren": "menarik",
+"jelek": "kurang baik",
+
+// perasaan (halus)
+"baper": "terbawa perasaan",
+"ilfeel": "hilang ketertarikan",
+"mager": "kurang bersemangat",
+"down": "merasa sedih",
+"panik": "cemas",
+"overthinking": "terlalu banyak berpikir",
+
+// aktivitas harian
+"nongkrong": "berkumpul",
+"curhat": "bercerita",
+"scroll": "menelusuri",
+"ngecek": "memeriksa",
+"nyari": "mencari",
+"nyiapin": "menyiapkan",
+"ngatur": "mengatur",
+
+// respon & reaksi
+"ok": "baik",
+"oke": "baik",
+"siap": "baik",
+"iya": "ya",
+"yap": "ya",
+"nah": "",
+"eh": "",
+
+// penekanan santai
+"banget": "sangat",
+"parah": "cukup berat",
+"lumayan": "cukup",
+
+// waktu & kondisi
+"pas": "ketika",
+"lagi": "sedang",
+"sempet": "sempat",
+"keburu": "terlanjur",
+  // ===== TAMBAHAN BATCH 2 =====
+
+// melihat & memperhatikan
+"ngeliat": "melihat",
+"ngelihat": "melihat",
+"mantengin": "memperhatikan",
+"ngintip": "mengamati",
+"melirik": "menoleh",
+"natap": "menatap",
+
+// berpikir & merasakan
+"ngerasa": "merasa",
+"ngira": "mengira",
+"mikir": "berpikir",
+"kepikiran": "terlintas dalam pikiran",
+"ngeh": "menyadari",
+"nyadar": "menyadari",
+"penasaran": "ingin tahu",
+
+// berbicara & komunikasi
+"ngomong": "berbicara",
+"bilang": "mengatakan",
+"ngucap": "mengucapkan",
+"jawab": "menjawab",
+"nanya": "bertanya",
+"nyeletuk": "berkata spontan",
+"bisik": "berkata pelan",
+"teriak": "berseru",
+
+// tindakan ringan
+"ngambil": "mengambil",
+"naro": "meletakkan",
+"nyimpen": "menyimpan",
+"buka": "membuka",
+"nutup": "menutup",
+"lempar": "melempar",
+"ngelakuin": "melakukan",
+
+// proses & keadaan
+"jalanin": "menjalani",
+"lewat": "melalui",
+"terjadi": "berlangsung",
+"mulai": "memulai",
+"berakhir": "selesai",
+"ketunda": "tertunda",
+
+// respon emosional
+"senyum": "tersenyum",
+"ketawa": "tertawa",
+"nangis": "menangis",
+"nghela": "menghela napas",
+"diem": "terdiam",
+
+// kebiasaan narasi santai
+"ujungnya": "pada akhirnya",
+"seketika": "secara tiba-tiba",
+"perlahan": "secara perlahan",
+"tanpa sadar": "tanpa disadari",
+  // ===== TAMBAHAN BATCH 3 =====
+
+// emosi batin
+"capek": "lelah",
+"cape": "lelah",
+"kecewa": "merasa kecewa",
+"sedih": "merasa sedih",
+"takut": "merasa takut",
+"degdeg": "gelisah",
+"waswas": "khawatir",
+"lega": "merasa lega",
+"bangga": "merasa bangga",
+"malu": "merasa malu",
+"bersalah": "merasa bersalah",
+
+// konflik perasaan
+"bimbang": "ragu-ragu",
+"galau": "diliputi kebingungan",
+"resah": "merasa tidak tenang",
+"kepo": "terlalu ingin tahu",
+"emosi": "terbawa perasaan",
+"sensi": "mudah tersinggung",
+
+// reaksi halus (cerpen-friendly)
+"nunduk": "menunduk",
+"noleh": "menoleh",
+"nghindar": "menghindar",
+"ngelirik": "melirik",
+"nahan": "menahan",
+"ngumpet": "bersembunyi",
+
+// keadaan mental
+"fokus": "berkonsentrasi",
+"konsen": "berkonsentrasi",
+"buyar": "tidak terarah",
+"kosong": "hampa",
+"penuh": "dipenuhi",
+
+// suasana & waktu
+"sunyi": "hening",
+"rame": "ramai",
+"gelap": "redup",
+"terang": "bercahaya",
+"pagi-pagi": "pagi hari",
+"malem": "malam hari",
+
+// narasi reflektif
+"pelan-pelan": "perlahan",
+"tiba-tiba": "mendadak",
+"tanpa mikir": "tanpa berpikir panjang",
+"entah kenapa": "tanpa alasan yang jelas",
+  // ===== TAMBAHAN BATCH 4 =====
+
+// sapaan & rujukan
+"kamu": "dirimu",
+"loe": "kamu",
+"lu": "kamu",
+"gue": "aku",
+"gw": "aku",
+"doi": "dia",
+
+// respons umum
+"iya": "ya",
+"iyah": "ya",
+"oke": "baik",
+"okey": "baik",
+"sip": "baiklah",
+"siap": "saya mengerti",
+
+// penolakan halus
+"ngga": "tidak",
+"enggak": "tidak",
+"nggk": "tidak",
+"ga bisa": "tidak bisa",
+"gabisa": "tidak bisa",
+"jangan": "sebaiknya tidak",
+
+// ekspresi perasaan
+"kangen": "merindukan",
+"sayang": "peduli",
+"kesel": "kesal",
+"sebel": "kesal",
+"takut banget": "sangat takut",
+"seneng": "senang",
+"bahagia": "merasa bahagia",
+
+// reaksi dialog
+"eh": "hei",
+"loh": "ternyata",
+"kok": "mengapa",
+"kenapa sih": "mengapa begitu",
+"masa": "benarkah",
+"yah": "sayangnya",
+
+// ajakan & permintaan
+"ayok": "ayo",
+"ayo dong": "mari",
+"tolong dong": "tolong",
+"bentar": "sebentar",
+"nunggu": "menunggu",
+
+// penegasan ringan
+"beneran": "sungguh",
+"serius": "dengan sungguh-sungguh",
+"jujur": "terus terang",
+"sebenernya": "sebenarnya",
+
+// penutup dialog
+"udah": "sudah",
+"selesai": "telah usai",
+"yaudah": "baiklah",
+"gapapa": "tidak masalah",
+  // ===== TAMBAHAN BATCH 5 =====
+
+// perasaan cinta
+"jatuh cinta": "mulai mencintai",
+"baper": "terbawa perasaan",
+"terlanjur sayang": "sudah terikat perasaan",
+"cinta banget": "sangat mencintai",
+"naksir": "menyukai diam-diam",
+
+// rindu & jarak
+"kangen banget": "sangat merindukan",
+"kangen parah": "teramat merindukan",
+"nungguin": "menanti",
+"lama ga ketemu": "sudah lama tidak bertemu",
+"jauh banget": "terasa sangat jauh",
+
+// kehilangan & luka
+"patah hati": "kehilangan perasaan utuh",
+"sakit banget": "terasa sangat menyakitkan",
+"kecewa": "merasa kecewa",
+"nyesek": "terasa sesak",
+"hancur": "merasa runtuh",
+
+// emosi halus
+"capek": "lelah",
+"cape": "lelah",
+"bingung": "merasa bimbang",
+"galau": "merasa resah",
+"kosong": "merasa hampa",
+"tenang": "merasa damai",
+
+// keintiman batin
+"diemin": "membiarkan",
+"nemenin": "menemani",
+"peluk": "merangkul",
+"pegangan": "saling menggenggam",
+"deket": "berada di dekat",
+
+// penerimaan & keikhlasan
+"ikhlas": "menerima dengan lapang",
+"lepasin": "melepaskan",
+"biarin": "membiarkan",
+"udah lah": "biarlah",
+"gapapa kok": "tidak apa-apa"
 };
 
 let highlightAktif = true;
